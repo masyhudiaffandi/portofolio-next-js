2330cfc48b986d76fb0484b8b9b098fc687f5495
Masyhudi Affandi <140623347+masyhudiaffandi@users.noreply.github.com>
2023-12-31 16:54:42 +0700

    feat:

--- app/components/button-main.tsx
+++ app/components/button-main.tsx
@@ -1,18 +1,27 @@
 'use client'
-import Link from 'next/link'
 
-const buttons = () => {
-    const handleDownload = () => {
-        const fileUrl = '@/public/cv.pdf';
-            window.open(fileUrl, '_blank');
-    }
-    return (
-        <>
-            <button className=' text-md 77md:text-lg px-6 text-center hover:bg-white text-white transition-all hover:text-[#676394] py-2 border border-solid border-white rounded-full' type='button' onClick={handleDownload}>Download CV</button>
-        </>
+// import Link from 'next/link';
 
-        
-    )
-}
+const Buttons = () => {
+  const handleDownload = () => {
+    const fileUrl = './cv.pdf'; // Adjust the path based on your project structure
+    const a = document.createElement('a');
+    a.href = fileUrl;
+    a.download = 'cv.pdf';
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+  };
 
-export default buttons
+  return (
+    <button
+      className='text-md px-6 text-center hover:bg-white text-white transition-all hover:text-[#676394] py-2 border border-solid border-white rounded-full'
+      type='button'
+      onClick={handleDownload}
+    >
+      Download CV
+    </button>
+  );
+};
+
+export default Buttons;
