2fa4d5e1640bcc838df6e338133ab57f4d33765f
masyhudiaffandi <masyhudiaffandi816@gmail.com>
2024-01-20 20:05:46 +0700

    feat: about

--- app/components/about.tsx
+++ app/components/about.tsx
@@ -15,6 +15,7 @@ const About = () => {
                         Understanding Laravel and React Native and possessing problem-solving and critical thinking skills to provide the best solutions when faced with challenges and efficiently complete assigned tasks. 
                         <br /><br />
                         Entrusted with the role of Front-end and Fullstack Developer in various web development projects at SMK Telkom Sidoarjo. Achieved 2nd place in the techfest competition in the Web Technology category held at the school. <br /><br />
+                        
                         With the experience and skills acquired, successfully developed various web projects such as the selection of Student Council Chairpersons, LuminaHealthHub, and other projects. Also achieved the accolade of 2nd place in the techfest competition at the school.
                         </p>
                     </div>
