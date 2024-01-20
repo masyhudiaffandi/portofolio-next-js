e53d76ea1641f8a67989343927a146e6b6a0cd3d
masyhudiaffandi <masyhudiaffandi816@gmail.com>
2024-01-20 20:05:20 +0700

    feat: about me

--- app/components/about.tsx
+++ app/components/about.tsx
@@ -3,8 +3,22 @@
 const About = () => {
     return (
         <section id="about">
-            <div className="pt-[24vh]">
-                <h1>Ini About</h1>
+            <div className="pt-[24vh] px-48">
+                <div className="title mb-8">
+                    <h1 className="text-3xl">About me</h1>
+                </div>
+                <div className="text">
+                    <div className="paragraph">
+                        <p>An eleventh-grade student at SMK Telkom Sidoarjo majoring in Telecommunication Access Network Engineering. Has experience as a Front-end and Fullstack Developer in developing various web projects at school. 
+                        <br />
+                        <br />
+                        Understanding Laravel and React Native and possessing problem-solving and critical thinking skills to provide the best solutions when faced with challenges and efficiently complete assigned tasks. 
+                        <br /><br />
+                        Entrusted with the role of Front-end and Fullstack Developer in various web development projects at SMK Telkom Sidoarjo. Achieved 2nd place in the techfest competition in the Web Technology category held at the school. <br /><br />
+                        With the experience and skills acquired, successfully developed various web projects such as the selection of Student Council Chairpersons, LuminaHealthHub, and other projects. Also achieved the accolade of 2nd place in the techfest competition at the school.
+                        </p>
+                    </div>
+                </div>
             </div>
         </section>
     )
