34c9d1f499a96cd30949381cb3ec7fb48560006a
masyhudiaffandi <masyhudiaffandi816@gmail.com>
2024-02-03 21:18:09 +0700

    coming soon

--- app/components/edu.tsx
+++ app/components/edu.tsx
@@ -3,8 +3,13 @@
 const Edu = () => {
     return(
         <section id="Edu">
-            <div className="pt-[24vh]">
-                <h1>Ini Edu</h1>
+            <div className="pt-[24vh] justify-center text-center">
+                <div className="title">
+                    <h1>Education</h1>
+                </div>
+                <div className="isi h-full flex justify-center items-center">
+                    <h1>Comming-soon</h1>
+                </div>
             </div>
         </section>
     )
